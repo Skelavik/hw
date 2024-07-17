9e89940d193285eb2a248938b1c25d03fe5cd73e
Skelavik <Skwlavik@yandex.ru>
2024-07-17 16:31:55 +0300

    added func show_all_candidate

--- hw11/hw11_2/main.py
+++ hw11/hw11_2/main.py
@@ -3,15 +3,18 @@ import utils
 
 app = Flask(__name__)
 
-@app.route("/<int:x>")
+@app.route("/")
+def show_all_candidate():
+    name_candidate = []
+    items = utils.load_candidates_from_json()
+    for item, item_name in items.items():
+        name_candidate.append((item_name["id"],item_name["name"]))
+    return render_template("list.html",items=name_candidate)
+
+@app.route("/candidate/<int:x>")
 def page_index(x):
     data = utils.get_candidate(x)
     return render_template("card.html",name_candidate=data["name"],positional_candidate=data["position"],skills_candidate=data["skills"],foto_candidate=data["picture"])
-
-@app.route("/")
-def show_all_candidate():
-    items = utils.load_candidates_from_json()
-    return render_template("list.html",items=items)
 
 
 

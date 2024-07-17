cdd3411c7c1be5719e456e5496fa7ef0f6c40137
Skelavik <Skwlavik@yandex.ru>
2024-07-17 13:14:42 +0300

    added no done show_all_candidate

--- hw11/hw11_2/main.py
+++ hw11/hw11_2/main.py
@@ -8,4 +8,11 @@ def page_index(x):
     data = utils.get_candidate(x)
     return render_template("card.html",name_candidate=data["name"],positional_candidate=data["position"],skills_candidate=data["skills"],foto_candidate=data["picture"])
 
+@app.route("/")
+def show_all_candidate():
+    items = utils.load_candidates_from_json()
+    return render_template("list.html",items=items)
+
+
+
 app.run()

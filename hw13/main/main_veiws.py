cc767e506c6a5658c216759b4c76a75f4e07d2f1
Aleksey <Skelavik@yandex.ru>
2024-10-13 19:42:32 +0300

    do index.

--- hw13/main/main_veiws.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, request
-
-main_blueprint = Blueprint("main_blueprint", __name__)
-
-@main_blueprint.route("/")
-def main_page():
-    return render_template('index.html')
